752e6342340c183e87b10ddd7246e0e93e6ef5f0
TomGnill <s.drun@yandex.ru>
2021-07-10 20:45:32 +0300

    refactor

--- Kysect.AssignmentReporter/Models/ReportExtendedInfo.cs
+++ Kysect.AssignmentReporter/Models/ReportExtendedInfo.cs
@@ -5,5 +5,14 @@
         public string Intro { get; set; }
         public string Conclusion { get; set; }
         public string Path { get; set; }
+
+        public ReportExtendedInfo() { }
+
+        public ReportExtendedInfo(string path, string intro, string conclusion)
+        {
+            Path = path;
+            Intro = intro;
+            Conclusion = conclusion;
+        }
     }
 }
